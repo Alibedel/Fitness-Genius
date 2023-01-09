600e5d6d17433dd3846ea7d28d3072fd5fee89d2
anhad sidhu <anhadsidhu96@gmail.com>
2023-01-08 16:59:48 -0800

    create step

--- main_app/models.py
+++ main_app/models.py
@@ -2,20 +2,14 @@ from django.db import models
 from datetime import date
 today = date.today()
 
-BODY = (
-    ('B', 'Back and Chest'),
-    ('L', 'Legs'),
-    ('A', 'Arms')
-)
+
 
 
 class Workout(models.Model):
 
       date = models.DateField('Workout date', default= today)
       body = models.CharField(
-    max_length=1,
-    choices=BODY,
-    default=BODY[0][0]
+    max_length=50,
   )
 
     #   def __str__(self):
