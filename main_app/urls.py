1a43b27d6681aeea5b426942c25dc3e355e4dda2
Ali Bedel <2xog@Alis-MacBook-Pro.local>
2023-01-10 19:48:24 -0500

    crud

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('workouts/', views.workouts_index, name='index'),
+  path('workouts/<int:workout_id>/', views.workouts_detail, name='detail'),
+  path('workouts/create/', views.WorkoutCreate.as_view(), name='workouts_create'),
+  path('workouts/<int:pk>/update/', views.WorkoutUpdate.as_view(), name='workouts_update'),
+  path('workouts/<int:pk>/delete/', views.WorkoutDelete.as_view(), name='workouts_delete'),
 ]
